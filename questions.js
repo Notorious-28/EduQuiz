d29d416aaea58bb86e0307eddfb2cba84f2ef838
Jhon Hendrix Polar <106592760+Notorious-28@users.noreply.github.com>
2023-11-04 20:56:56 +0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,222 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for in web development?",
+        answer: "C. HyperText Markup Language",
+        options: [
+            "A. Hyperlink and Text Markup Language",
+            "B. Hyper Transfer Markup Language",
+            "C. HyperText Markup Language",
+            "D. Hyper Tool Markup Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which programming language is commonly used for server-side scripting in web development?",
+        answer: "C. PHP",
+        options: [
+            "A. JavaScript",
+            "B. Python",
+            "C. PHP",
+            "D. Ruby"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does CSS stand for in web development?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Computer Style Sheet",
+            "C. Creative Style Sheet",
+            "D. Colorful Style Sheet"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which of the following is not a version control system commonly used in web development?",
+        answer: "D. JIRA",
+        options: [
+            "A. Git",
+            "B. SVN (Subversion)",
+            "C. CVS (Concurrent Versions System)",
+            "D. JIRA"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is the purpose of a 'favicon' in web development?",
+        answer: "B. It's a small icon displayed in the browser tab or bookmark list.",
+        options: [
+            "A. It's a security feature to prevent cross-site scripting attacks.",
+            "B. It's a small icon displayed in the browser tab or bookmark list.",
+            "C. It's a type of web server.",
+            "D. It's a markup language for defining document structure"
+        ]
+    },
+    {
+        numb: 6,
+        question: "In web development, what does the term 'HTTP' stand for?",
+        answer: "A. HyperText Transfer Protocol",
+        options: [
+            "A. HyperText Transfer Protocol",
+            "B. Hyperlink Transfer Text Protocol",
+            "C. Hypertext and Text Transfer Protocol",
+            "D. Hyperlink and Text Transfer Protocol"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Which programming language is often used for adding interactivity and behavior to web pages in the client-side?",
+        answer: "C. JavaScript",
+        options: [
+            "A. PHP",
+            "B. Java",
+            "C. JavaScript",
+            "D. Python"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What is the purpose of a CSS framework in web development?",
+        answer: "B. To provide pre-designed, reusable styles and layouts.",
+        options: [
+            "A. To define server configurations.",
+            "B. To provide pre-designed, reusable styles and layouts.",
+            "C. To secure a web application against attacks.",
+            "D. To manage databases."
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is the term for a collection of related web pages linked together and accessible via a common URL?",
+        answer: "A. Website",
+        options: [
+            "A. Website",
+            "B. Web application",
+            "C. Web server",
+            "D. Web directory"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Which of the following is a commonly used database management system in web development?",
+        answer: "B. MySQL",
+        options: [
+            "A. Photoshop",
+            "B. MySQL",
+            "C. Node.js",
+            "D. Bootstrap"
+        ]
+    },
+    {
+        numb: 11,
+        question: "What does CPU stand for in computer science?",
+        answer: "C. Central Processing Unit",
+        options: [
+            "A. Central Process Unit",
+            "B. Central Programming Unit",
+            "C. Central Processing Unit",
+            "D. Central Print Unit"
+        ]
+    },
+    {
+        numb: 12,
+        question: "Which programming language is often used for artificial intelligence and machine learning applications?",
+        answer: "A. Python",
+        options: [
+            "A. Python",
+            "B. Java",
+            "C. C++",
+            "D. Ruby"
+        ]
+    },
+    {
+        numb: 13,
+        question: "What is the term for a programming error that causes a program to terminate abnormally?",
+        answer: "D. Crash",
+        options: [
+            "A. Debug",
+            "B. Syntax error",
+            "C. Exception",
+            "D. Crash"
+        ]
+    },
+    {
+        numb: 14,
+        question: "Which data structure follows the Last-In, First-Out (LIFO) principle?",
+        answer: "B. Stack",
+        options: [
+            "A. Queue",
+            "B. Stack",
+            "C. Array",
+            "D. Linked List"
+        ]
+    },
+    {
+        numb: 15,
+        question: "In computer science, what is the role of an algorithm?",
+        answer: "C. To solve a problem step-by-step",
+        options: [
+            "A. To create a user interface",
+            "B. To optimize database queries",
+            "C. To solve a problem step-by-step",
+            "D. To store data securely"
+        ]
+    },
+    {
+        numb: 16,
+        question: "What is the purpose of an operating system in a computer?",
+        answer: "A. To run application software",
+        options: [
+            "A. To run application software",
+            "B. To format data for storage",
+            "C. To design websites",
+            "D. To calculate mathematical functions"
+        ]
+    },
+    {
+        numb: 17,
+        question: "Which programming language is known for its use in web development for both client and server-side scripting?",
+        answer: "C. JavaScript",
+        options: [
+            "A. Python",
+            "B. Ruby",
+            "C. JavaScript",
+            "D. C#"
+        ]
+    },
+    {
+        numb: 18,
+        question: "What does HTML5 stand for in web development?",
+        answer: "A. Hypertext Markup Language 5",
+        options: [
+            "A. Hypertext Markup Language 5",
+            "B. Hyper Transfer Markup Language 5",
+            "C. High-Throughput Markup Language 5",
+            "D. Hyperlink and Text Markup Language 5"
+        ]
+    },
+    {
+        numb: 19,
+        question: "What type of computer memory is non-volatile and used for long-term data storage?",
+        answer: "C. Flash memory",
+        options: [
+            "A. RAM (Random Access Memory)",
+            "B. ROM (Read-Only Memory)",
+            "C. Flash memory",
+            "D. Cache memory"
+        ]
+    },
+    {
+        numb: 20,
+        question: "Which computer scientist is considered the 'father of modern computer science' and contributed to the development of the Turing machine?",
+        answer: "A. Alan Turing",
+        options: [
+            "A. Alan Turing",
+            "B. Ada Lovelace",
+            "C. Charles Babbage",
+            "D. John von Neumann"
+        ]
+    }
+];
